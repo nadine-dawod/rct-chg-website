5120d56bc36b771569f2bb2ac5970011f215b252
Nadine <nadinekdawod@gmail.com>
2024-01-31 17:04:57 +0100

    Updates

--- src/Components/ReusableButton.js
+++
@@ -1,8 +0,0 @@
-
-
-const ReusableButton = ({children, handleClick}) => {
-    return <button className="ReusableBtn" onClick={handleClick}>{children}</button>
-  };
-
-
-  export default ReusableButton;
